df6d20b8e991cb4bb1efbdd272c7817f65734958
victorhol <137225990+victorhol@users.noreply.github.com>
2026-01-21 14:52:43 +0100

    Change output file from details_csv to summary_csv
    Updated command to output summary CSV instead of details CSV.

--- run_experiment.py
+++ run_experiment.py
@@ -1,25 +1,19 @@
 import os
 
-# Konfiguration
-runs = 3
-generations = 35
+runs = 16
+generations = 25
 
-# Vi vil teste disse 3 scenarier
-scenarios = [1, 3, 5]  # Top 1, Top 3, Top 5
-
-print("Starter eksperimentet...")
+top_agents = [1, 3, 5]
 
 for n in scenarios:
-    print(f"\n--- Starter simulering for Top {n} ---")
+    print(f"---Top {n}---")
     
-    # Vi giver filen et unikt navn, f.eks. "summary_top1.csv"
-    cmd = (f"python opdateret_sim.py "
+    cmd = (f"python agent_plays.py "
            f"--runs {runs} "
            f"--generations {generations} "
            f"--elite_n {n} "
            f"--summary_csv data_top{n}.csv "
-           f"--details_csv details_top{n}.csv")
     
     os.system(cmd)
 
-print("\nAlle eksperimenter er færdige! Data ligger i data_top1.csv, data_top3.csv, osv.")
+print("\nThe eksperiment is done. The data can be found in the data_topN.csv file(s)")
